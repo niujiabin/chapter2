f7c0e0cf0de129376d4a1d7aa3a48a6c7a3a1998
niujiabin <njb19920913@163.com>
2017-12-29 15:49:27 +0800

    ok

---
+++ src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
@@ -0,0 +1,11 @@
+package org.smart4j.chapter2.controller;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Created by yaphetsBin on 2017-12-29.
+ */
+@WebServlet
+public class CustomerCreateServlet {
+
+}
